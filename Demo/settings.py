38963432b23e197057ea84797e82327bfec239d0
Adithya V Krishna <adithyavkrishna@Adithyas-MacBook-Air.local>
2024-03-06 20:38:25 +0530

    allowed_hosts

--- Demo/settings.py
+++ Demo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--fa39&tci872g29^n4k6cy-&f77gztwiu0_+hz%m9)vwb#^o9y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
